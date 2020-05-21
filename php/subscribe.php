c9d421f8bdbad49997bb7b134917f493f67e2968
Mladshev_Evgeniy <Fixter.spb@gmail.com>
2020-05-21 19:30:46 +0300

    Сверстаны страницы: index.php, product.php, single.php

---
+++ php/subscribe.php
@@ -0,0 +1,27 @@
+<div class="subscribe center">
+    <div class="subscribe_slider">
+        <div class="subscribe_slider_content">
+            <img src="img/photo_83.png" alt="photo_83" class="photo_83">
+            <div class="suscribe_text_box">
+                <p class="subscribe_text">“Vestibulum quis porttitor dui! Quisque viverra nunc mi, a
+                    pulvinar purus condimentum a. Aliquam condimentum mattis neque sed pretium”</p>
+                <p class="subscribe_name">Bin Burhan</p>
+                <p class="dnaka">Dnaka, Bd</p>
+            </div>
+        </div>
+        <div class="pagination">
+            <a href="#" class="pagination_link"></a>
+            <a href="#" class="pagination_link pagination_link_active"></a>
+            <a href="#" class="pagination_link"></a>
+        </div>
+    </div>
+
+    <div class="subscribe_email_box">
+        <h4 class="subscribe_title">Subscribe</h4>
+        <p class="subscribe_email_text">FOR OUR NEWLETTER AND PROMOTION</p>
+        <form action="#" class="email_form">
+            <input type="text" name="email" id="email" class="input_email" placeholder="Enter Your Email">
+            <a href="#" class="button button_subscribe">Subscribe</a>
+        </form>
+    </div>
+</div>
