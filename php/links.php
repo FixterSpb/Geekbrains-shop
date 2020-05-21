74f9a8804154616b2a67875c965b621c51d5f121
Mladshev_Evgeniy <Fixter.spb@gmail.com>
2020-05-21 15:29:03 +0300

    Практические задания к урокам 2 и 3

---
+++ php/links.php
@@ -0,0 +1,6 @@
+<link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,400&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="style.css?ver=1.10">
